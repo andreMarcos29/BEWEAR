87cfcc5e0ba10fc9a4c58efb36f48c2c32c5f54a
andreMarcos29 <andremarcosdeoliveira2010@gmail.com>
2025-08-07 12:20:54 +0100

    feat: implementar sistema de autenticação e gerenciamento de carrinho; adicionar componentes de UI e lógica de produtos

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,13 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/.next/types/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
   {
     plugins: {
       "simple-import-sort": simpleImportSort,
@@ -22,5 +18,3 @@ const eslintConfig = [
     },
   },
 ];
-
-export default eslintConfig;
